5e5d7989eb987ee94bcd55cafddda1a0bc9dafcd
Keith Fuller <36208707+keith66fuller@users.noreply.github.com>
2018-06-24 13:30:02 -0500

    sunday at starbucks

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -56,105 +56,82 @@ $(document).ready(function () {
     ]);
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Constants
-    const questionTime = 4; // # of seconds between questions
-    const dialogTime = 10; // # of seconds that will elapse before we close a modal after waiting on you to close it.
+    const questionTime = 6; // # of seconds between questions
+    const dialogTime = 4; // # of seconds that will elapse before we close a modal after waiting on you to close it.
     let aTimer; // The global timeout timer for the modal window.
-    let sTimer;
+    let numRight;
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    function askQuestionByIndex(idx) {
-        return new Promise(async (resolve, reject) => {
-            const question = questions[idx]
-            $('#question').text(question.question)
-            $('#answers').empty();
-            question.answers.forEach((answer, i) => {
-                $('#answers').append(
-                    $('<tr>')
-                        .on('click', function (e) {
-                            while (aTimer--) {
-                                console.log(`ATIMER ${aTimer}`)
-                                window.clearTimeout(aTimer); // will do nothing if no timeout with id is present
-                            }
-                            $('#answers').off();
-                            if ($(this).data("number") == question.correctAnswer) {
-                                numRight++;
-                                return resolve('Correct!')
-                            } else {
-                                return resolve('Sorry, wrong answer ...')
-                            }
-                        })
-                        .attr("data-number", i).append(
-                            $('<td>').addClass("answer").text(answer)
-                        )
-                )
-            });
-
-
-
-
-
-            // time remaining counter
-            let seconds = questionTime
-            aTimer = setInterval(function () {
-                console.log(`INTERVAL ${seconds}`)
-                seconds--;
-                $('#timeRemaining').empty().append(`Time Remaining: ${seconds}`)
-                if (seconds < 0) {
-                    console.log('CLEAR INTERVAL')
-                    clearInterval(aTimer);
-                    return resolve('Times UP!!!!')
-                }
-            }, 1000)
-
-        })
-    }
-
-    function showModal(title, message) {
-        return new Promise(async (resolve) => {
-            [[$('#modalTitle'), title], [$('#modalMsg'), message]].map(e => { $(e[0]).empty().text(e[1]) });
-            $('#theModal').modal('show')
-            $('#modalOk').on('click', resolve())
-        })
-    }
-
-    
-    function waitResolve(t) {
+    function showModal(title, message, delay) {
+        [[$('#modalTitle'), title], [$('#modalMsg'), message]].map(e => { $(e[0]).empty().text(e[1]) });
+        $('#theModal').modal('show')
         return new Promise((resolve, reject) => {
-            $('#modalOk').on('click', () => reject());
-            setTimeout(() => {
+            $('#modalOk').on('click', () => resolve());
+            if (delay) {
+                setTimeout(() => {
+                    resolve();
+                }, dialogTime * 1000)
+            }
+        })
+            .then(() => {
                 $('#modalOk').off('click');
-                resolve('resolved');
-            }, t * 1000);
-        });
+                $('#theModal').modal('hide');
+            });
+    }
+    function gameOver() {
+        return new Promise(resolve => {
+            showModal('Game Over', `${(numRight == questions.length) ? "You Won!" : "You Lost!"}\n${numRight} out of ${questions.length} correct.`, false)
+        })
+            .then(initGame)
     }
 
-
-    async function playTheGame(qIdx) {
-        console.log('PLAY GAME')
-        let question = questions[qIdx];
-        console.log('ASK QUESTION')
-        var x = await askQuestionByIndex(qIdx)
-        .catch(() => console.log('AQBI ERR ${err}'))
-        console.log('SHOW MODAL')
-        var y = await showModal('foobar', `The answer was\n${question.answers[question.correctAnswer]}`)
-        .catch((err) => console.log(`SHOW MODAL ERR ${err}`))
-        console.log('WAITRESOLVE')
-        var z = await waitResolve(dialogTime)
-        .catch((err) => console.log(`WAITRESOLVE ERR ${err}`))
-        $('#theModal').modal('hide');
-        console.log("CLEAR TIMEOUT")
-        console.log(`QIDX: ${qIdx}`)
-
-        clearTimeout(aTimer)
-        if (--qIdx >= 0) {
-            playTheGame(qIdx)
-        } else {
-            $('#modalMsg').empty().text(`${(numRight == questions.length) ? "You Won!" : "You Lost!"}\n${numRight} out of ${questions.length}`);
-            // $('#theModal').modal('show');
-            // $('#modalOk').on('click', () => { initGame() });
+    async function initGame() {
+        numRight = 0
+        for (let i = 0; i < questions.length; i++) {
+            let question = questions[i];
+            await new Promise(async (resolve, reject) => {
+                $('#question').text(question.question)
+                $('#answers').empty();
+                question.answers.forEach((answer, i) => {
+                    $('#answers').append(
+                        $('<tr>')
+                            .on('click', function (e) {
+                                clearInterval(aTimer); 
+                                $('#answers').off();
+                                if ($(this).data("number") == question.correctAnswer) {
+                                    numRight++;
+                                    return resolve('Correct!')
+                                } else {
+                                    return resolve('Sorry, wrong answer ...')
+                                }
+                            })
+                            .attr("data-number", i).append(
+                                $('<td>').addClass("answer").text(answer)
+                            )
+                    )
+                });
+                // time remaining counter
+                let seconds = questionTime
+                aTimer = setInterval(function () {
+                    console.log(`INTERVAL ${seconds} ATIMER ${aTimer}`)
+                    $('#timeRemaining').empty().append(`Time Remaining: ${seconds--}`)
+                    if (seconds < 0) {
+                        console.log('CLEAR INTERVAL')
+                        $('#timeRemaining').empty()
+                        clearInterval(aTimer);
+                        return resolve('Times UP!!!!')
+                    }
+                }, 1000)
+            })
+                .then(async result => {
+                    clearTimeout(aTimer)
+                    console.log(`RESULT: ${result}`)
+                    await showModal(result, `The answer was\n${question.answers[question.correctAnswer]}`, true)
+                })
+            clearTimeout(aTimer)
         }
+        await gameOver();
     }
-
 
     /*ooo        ooooo            .o.            ooooo      ooooo      ooo 
     `88.       .888'           .888.           `888'      `888b.     `8' 
@@ -171,11 +148,6 @@ $(document).ready(function () {
 
     $('#theModal').modal('hide')
 
-    let numRight = 0
-    function initGame() {
-        numRight = 0
-        playTheGame(questions.length - 1)
-    }
 
     initGame();
 })
