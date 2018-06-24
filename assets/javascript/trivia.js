7d95ca737c5dd9bdd380135449cd971eebb3a76d
Keith Fuller <36208707+keith66fuller@users.noreply.github.com>
2018-06-23 21:14:18 -0500

    now its all hosed

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -4,19 +4,6 @@ $(document).ready(function () {
         return o;
     };
 
-    function ask(idx) {
-        $('#question').text(questions[idx].question)
-        $('#answers').empty();
-        questions[idx].answers.forEach(answer => {
-            $('#answers').append(
-                $('<li>').append(
-                    $('<h5>').addClass("answer").text(answer)
-                )
-            )
-        });
-        return questions[idx]
-    };
-
     const questions = shuffle([
         {
             question: "What TV show featured the A.D narrator as a breakout child star?",
@@ -26,7 +13,7 @@ $(document).ready(function () {
                 "The Andy Griffith Show",
                 "Petticoat Junction"
             ],
-            correctAnswer: "The Andy Griffith Show"
+            correctAnswer: 2
         },
         {
             question: "What A.D. actor got caught up in #MeToo?",
@@ -39,7 +26,7 @@ $(document).ready(function () {
                 "Michael Cera",
                 "Tony Hale"
             ],
-            correctAnswer: "Jeffrey Tambor"
+            correctAnswer: 3
         },
         {
             question: "The show was sued by the ____ group Arrested Development over the use of its name",
@@ -51,7 +38,7 @@ $(document).ready(function () {
                 "Jazz",
                 "Classic Rock"
             ],
-            correctAnswer: "Hip-Hop"
+            correctAnswer: 1
         },
         {
             question: "A.D. never won any ____.",
@@ -64,91 +51,137 @@ $(document).ready(function () {
                 "Television Critics Association Awards",
                 "Writers Guild of America Awards"
             ],
-            correctAnswer: "Screen Actors Guild Awards"
-        },
+            correctAnswer: 4
+        }
     ]);
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Constants
-    let questionTime = 10; // # of seconds between questions
-    let dialogTime = 4; // # of seconds that will elapse before we close a modal after waiting on you to close it.
-
+    const questionTime = 4; // # of seconds between questions
+    const dialogTime = 10; // # of seconds that will elapse before we close a modal after waiting on you to close it.
+    let aTimer; // The global timeout timer for the modal window.
+    let sTimer;
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-    $('#theModal').modal('hide')
-
     function askQuestionByIndex(idx) {
-        return new Promise((resolve, reject) => {
-            let question = ask(idx);
-            $('#answers').on('click', '.answer', function (event) {
-                clearTimeout(aTimer);
-                $('#answers').off();
-                console.log($(this).text());
-                console.log(question.correctAnswer)
-                if ($(this).text() == question.correctAnswer) {
-                    return resolve($(this).text())
-                } else {
-                    return resolve(false)
-                }
-            })
+        return new Promise(async (resolve, reject) => {
+            const question = questions[idx]
+            $('#question').text(question.question)
+            $('#answers').empty();
+            question.answers.forEach((answer, i) => {
+                $('#answers').append(
+                    $('<tr>')
+                        .on('click', function (e) {
+                            while (aTimer--) {
+                                console.log(`ATIMER ${aTimer}`)
+                                window.clearTimeout(aTimer); // will do nothing if no timeout with id is present
+                            }
+                            $('#answers').off();
+                            if ($(this).data("number") == question.correctAnswer) {
+                                numRight++;
+                                return resolve('Correct!')
+                            } else {
+                                return resolve('Sorry, wrong answer ...')
+                            }
+                        })
+                        .attr("data-number", i).append(
+                            $('<td>').addClass("answer").text(answer)
+                        )
+                )
+            });
+
+
+
 
 
             // time remaining counter
-            let seconds = questionTime;
-            let aTimer = setInterval(function () {
-                $('#timeRemaining').empty();
-                $('#timeRemaining').append(`Time Remaining: ${seconds}`);
-                --seconds;
+            let seconds = questionTime
+            aTimer = setInterval(function () {
+                console.log(`INTERVAL ${seconds}`)
+                seconds--;
+                $('#timeRemaining').empty().append(`Time Remaining: ${seconds}`)
                 if (seconds < 0) {
-                    clearTimeout(aTimer);
-                    return resolve("TIMEOUT")
+                    console.log('CLEAR INTERVAL')
+                    clearInterval(aTimer);
+                    return resolve('Times UP!!!!')
                 }
             }, 1000)
 
         })
     }
-    function showModalThenHide(title, message, idx) {
-        $('#modalTitle').empty();
-        $('#modalTitle').text(title);
-        $('#modalMsg').empty();
-        $('#modalMsg').text(message);
-        $('#theModal').modal('show')
-        setTimeout(function () {
-            $(theModal).modal('hide')
 
-            if (--idx >= 0) {
-                playTheGame(idx)
-            } else {
-                let gameOvermsg = (numRight == questions.length) ? "You Won!" : "You Lost!"
-                $('#modalMsg').empty()
-                $('#modalMsg').text(gameOvermsg)
-                $('#theModal').modal('show')
-                $('#modalOk').on('click', function (event) {
-                    initGame();
-                })
-            }
-        }, dialogTime * 1000);
+    function showModal(title, message) {
+        console.log('SHOW MODAL')
+        return new Promise(async (resolve, reject) => {
+            [[$('#modalTitle'), title], [$('#modalMsg'), message]].map(e => { $(e[0]).empty().text(e[1]) });
+            $('#theModal').modal('show').on('click', resolve())
+        })
+    }
+    
+    
+    function showModalThenHide(title, message) {
+        console.log('SHOW MODAL')
+        return new Promise(async (resolve, reject) => {
+            [[$('#modalTitle'), title], [$('#modalMsg'), message]].map(e => { $(e[0]).empty().text(e[1]) });
+            $('#theModal').modal('show').on('click', resolve())
+
+
+            let seconds = dialogTime
+            sTimer = setInterval(function () {
+                console.log(`S-INTERVAL ${sTimer} ${seconds}`)
+                seconds--
+                if (seconds < 0) {
+                    console.log("HIDE MODAL")
+                    clearInterval(sTimer)
+                    $('#theModal').modal('hide');
+                    return resolve('DONE')
+                }
+            }, 1000)
+
+
+
+
+
+        })
     }
     async function playTheGame(qIdx) {
+        console.log('PLAY GAME')
         let question = questions[qIdx];
-        askQuestionByIndex(qIdx)
-            .then(result => {
-                console.log(`RESULT: ${result}`)
-                console.log(`CORRECT: ${question.correctAnswer}`)
-                switch (result) {
-                    case question.correctAnswer:
-                        showModalThenHide('Correct!', "The answer was\n" + question.correctAnswer, qIdx);
-                        numRight++;
-                        break;
-                    case "TIMEOUT":
-                        showModalThenHide('Times UP!!!!', "The answer was\n" + question.correctAnswer, qIdx);
-                        break;
-                    default:
-                        showModalThenHide('Sorry, wrong answer ...', 'The answer was\n' + question.correctAnswer);
-                        break;
-                }
-            })
+        var x = await askQuestionByIndex(qIdx)
+        console.log('QUESTION ASKED')
+        
+        var y = await showModal('foobar', `The answer was\n${question.answers[question.correctAnswer]}`)
+        var z = await waitResolve(dialogTime)
+        $('#theModal').modal('hide');
+        console.log("CLEAR TIMEOUT")
+        clearTimeout(aTimer)
+        if (--qIdx >= 0) {
+            playTheGame(qIdx)
+        } else {
+            $('#modalMsg').empty().text(`${(numRight == questions.length) ? "You Won!" : "You Lost!"}\n${numRight} out of ${questions.length}`)
+            $('#theModal').modal('show').on('click', () => { initGame() });
+        }
     }
+
+    function waitResolve(t) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve('resolved');
+            }, t * 1000);
+        });
+    }
+
+    /*ooo        ooooo            .o.            ooooo      ooooo      ooo 
+    `88.       .888'           .888.           `888'      `888b.     `8' 
+     888b     d'888           .8"888.           888        8 `88b.    8  
+     8 Y88. .P  888          .8' `888.          888        8   `88b.  8  
+     8  `888'   888         .88ooo8888.         888        8     `88b.8  
+     8    Y     888        .8'     `888.        888        8       `888  
+    o8o        o888o      o88o     o8888o      o888o      o8o        `8  
+                                                                         
+                                                                         
+                                                                         */
+
+    $('#theModal').modal('hide')
 
     let numRight = 0
     function initGame() {
